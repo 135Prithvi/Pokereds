06a37017c004e2d7ce7a10fe4133932630ddb1cb
135Prithvi <prithvi128717@gmail.com>
2022-05-20 12:23:34 +0530

    updating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next'
 import Link from 'next/link';
 import Image from 'next/image';
+import React from 'react';
+import Search from '../components/Search'
 const Home: NextPage = ({data}:any) => {
-  
+ 
   
   return (
     <div className="mx-10 sm:mx-20">
-      <div className="my-10 space-y-5 text-center sm:grid sm:grid-cols-2 sm:gap-x-4  sm:gap-y-5 sm:space-y-0">
+      <Search/>
+      <div className="my-5 space-y-5 text-center sm:grid sm:grid-cols-2 sm:gap-x-8  sm:gap-y-5 sm:space-y-0">
       {data.results.map((pokemon:any , _index: any) => {
-            return <Link href="/[id]" as={`/${_index+1}`} key={pokemon?.name}><div className=" cursor-pointer max-w-sm rounded-lg border-2 border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
+            return <Link href="/[id]" as={`/${_index+1}`} key={pokemon?.name}><div className=" cursor-pointer max-w-full rounded-lg border-2 border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
             <a >
               <Image
                 className="rounded-lg m-auto "
@@ -37,7 +40,6 @@ const Home: NextPage = ({data}:any) => {
 export default Home
 
 export async function getServerSideProps(_context: any) {
-  
   const URL = `https://pokeapi.co/api/v2/pokemon/?limit=494`
   const response = await fetch(URL)
   const data = await response.json()
